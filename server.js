1b643f6952b5fe5eae1d1b876640b4ec63229f6c
Simplejul <simplejul@protonmail.com>
2018-06-25 15:14:26 +0200

    page avec livres

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express = require('express');
 const app = express();
 const pug = require('pug');
 const sqlite3 = require('sqlite3').verbose();
+const bodyparser = require('body-parser');
 
 
-// Base de Données
+// Connexion a la BDD
 let db = new sqlite3.Database('./media', sqlite3.OPEN_READWRITE, (err) =>{   
     if (err) {
     return console.error(err.message);
@@ -12,17 +13,18 @@ let db = new sqlite3.Database('./media', sqlite3.OPEN_READWRITE, (err) =>{
   console.log('Connected to the "media" SQlite3 database.')
 });
 
-let sql = `SELECT bookID as id, name
+// Definittion de la requete
+let sql = `SELECT name, author, bookCount as av
             FROM book`;
 
+let lb=[];
 db.serialize(() => {
-    db.all(sql, (err, row) => {
+    db.each(sql, (err, row) => {
         if (err) {
-        console.error(err.message);
-        } else {
-            console.log("Pas de données")
+            console.error(err.message);
         }
-      console.log(row.id + "\t" + row.name)
+      console.log(row.name + "\t" + row.author + "\t" + row.av),
+      lb.push(row.name + " de " + row.author + " disponibilité: " + row.av);
     });
 });
    
@@ -30,7 +32,7 @@ db.serialize(() => {
 
 // Route racine
 app.get('/', (req,res) =>{
-    res.render('index', {title:'Media', book: 'ok', listbook:sql})
+    res.render('index', {title:'Media', book: 'ok', listbook:lb})
 })
 
 db.close((err) => {
