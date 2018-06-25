cfa99c759feda980ae2b7a973b7ffc65af9af561
Simplejul <simplejul@protonmail.com>
2018-06-25 17:00:18 +0200

    final

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
+// Je me suis arreté sur le req.body qui ne fonctionne pas.
+
 const express = require('express');
 const app = express();
 const pug = require('pug');
 const sqlite3 = require('sqlite3').verbose();
 const bodyparser = require('body-parser');
 
-
+//Utilisation de body-parser par le serveur
+app.use(bodyparser.urlencoded({
+    extended: false
+}));
 // Connexion a la BDD
 let db = new sqlite3.Database('./media', sqlite3.OPEN_READWRITE, (err) =>{   
     if (err) {
@@ -17,6 +22,7 @@ let db = new sqlite3.Database('./media', sqlite3.OPEN_READWRITE, (err) =>{
 let sql = `SELECT name, author, bookCount as av
             FROM book`;
 
+let countof=[];
 let lb=[];
 db.serialize(() => {
     db.each(sql, (err, row) => {
@@ -24,17 +30,40 @@ db.serialize(() => {
             console.error(err.message);
         }
       console.log(row.name + "\t" + row.author + "\t" + row.av),
-      lb.push(row.name + " de " + row.author + " disponibilité: " + row.av);
+      lb.push(row.name + " de " + row.author + " disponibilité: " + row.av),
+      countof.push(row.av);
+      console.log('countof c ca: '+ countof)      
     });
 });
-   
 
+console.log('count c encore ca ' + countof[0]);
+
+let bcount = 'SELECT bookCount FROM book WHERE bookID = ' + countof[0] + ';';
+console.log('bcount c est ca : ' + bcount);
+
+let changeCount = `UPDATE book
+            SET bookCount = ?
+            WHERE name = ?`;
+/*s
+db.run(sql, data, function(err) {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log(`Row(s) updated: ${this.changes}`);
+ 
+});*/
 
 // Route racine
 app.get('/', (req,res) =>{
-    res.render('index', {title:'Media', book: 'ok', listbook:lb})
+    res.render('index', {title:'Media', listbook:lb, nb:countof})
 })
 
+// Routes
+app.get('/resa',(req,res)=>{
+    console.log('le body parser: ' + req.body.bs),
+    res.send('La RESA')
+    
+})
 db.close((err) => {
     if (err) {
       console.error(err.message);
